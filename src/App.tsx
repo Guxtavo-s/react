b096f89000fe0222e6752e61c54d2b1cc355154e
Gustavo Oliveira <ghenrique.oliveira@outlook.com>
2022-09-29 14:11:55 -0300

    Menu, Rodapé e Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import Home from './components/home/Home';
+import {Grid} from '@material-ui/core';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
 
 function App() {
   return (
-  <Home/>
+  <>
+    <Navbar />
+    <Home />
+    <Footer />
+  </>
   );
 }
 
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,36 @@
-import React from "react"
+import React from 'react';
+import {Typography, Box, Grid, Button} from '@material-ui/core';
 import './Home.css';
 import foto from './imgs/foto1.png';
 
 function Home() {
     return (
         <>
-        <div className="config">
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Faça as suas postagens aqui!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src={foto} alt="Imagem" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+export default Home;
+
+{/* <div className="config">
             <div>
                 <h1 className="titulo">Seja bem vindo(a)!</h1>
                 <p className="pConfig"> Faça aqui as suas postagens!</p>
@@ -13,9 +38,4 @@ function Home() {
             <div className="fotoD">
                 <img src={foto} alt="foto" className="img" />
             </div>
-        </div>
-        </>
-    )
-}
-
-export default Home;
+        </div> */}
