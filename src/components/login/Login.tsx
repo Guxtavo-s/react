6a29020f43a074aa62b48ba7faef63d63e6e9e85
Gustavo Oliveira <ghenrique.oliveira@outlook.com>
2022-10-01 01:37:46 -0300

    Rotas

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import './Login.css';
-import { makeStyles } from '@material-ui/core/styles';
 
 function Login() {
 
